cda946abda61a0d6b844d6566c483b0179940a32
thespacedoctor <nothingbutdave@gmail.com>
2016-06-20 08:26:28 +0100

    x

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='panstamps',
       author_email='davidrobertyoung@gmail.com',
       license='MIT',
       packages=find_packages(),
-      package_data={'panstamps': ['resources/*/*', 'resources/*.*']},
+      # package_data={'panstamps': ['resources/*/*', 'resources/*.*']},
       include_package_data=True,
       install_requires=[
           'pyyaml',
