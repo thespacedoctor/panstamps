e220c5d236274dc53dc74a51cbbf049ca2e90d2a
thespacedoctor <nothingbutdave@gmail.com>
2017-06-30 16:35:56 +0100

    adding coverage badge

--- docs/build/html/_sources/_api/index.txt
+++ docs/build/html/_sources/_api/index.txt
@@ -1,5 +1,5 @@
 .. panstamps documentation master file, created by
-   sphinx-quickstart on Fri Jun 30 16:31:19 2017.
+   sphinx-quickstart on Fri Jun 30 16:35:38 2017.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- docs/source/_api/conf.py
+++ docs/source/_api/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # panstamps documentation build configuration file, created by
-# sphinx-quickstart on Fri Jun 30 16:31:19 2017.
+# sphinx-quickstart on Fri Jun 30 16:35:38 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
